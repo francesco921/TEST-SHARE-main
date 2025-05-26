550fd2fd60e700451640622c40aa2cf1595d3bee
francesco921 <erba.francesco.mp@gmail.com>
2025-05-26 13:54:04 +0700

    ***

--- pages/home.tsx
+++ pages/home.tsx
@@ -40,7 +40,7 @@ export default function HomePage() {
               href="https://whop.com/shrm/"
               className="bg-white text-blue-600 border border-blue-600 font-semibold px-6 py-3 rounded-lg shadow hover:bg-blue-50 transition"
             >
-              Community Support
+              24/7 Support
             </a>
           </div>
         </div>
